5ce31822fa2a2e31d3fb6478d24d117b475891a1
kraftwerk1611 <kraftwerk1611@gmail.com>
2017-12-01 17:16:36 -0500

    ver 2.0: Command Line app. The browser is launched based on CL argument passed. Tested with IE, FF and Chrome.

--- SeleniumCS-testBrowsers/Hooks.cs
+++ SeleniumCS-testBrowsers/Hooks.cs
@@ -3,34 +3,51 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Opera;
+using System;
 
 namespace SeleniumCS_testBrowsers
 {
     class Hooks
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            IWebDriver driver = new FirefoxDriver();
-            System.Console.WriteLine(driver.GetType().ToString());
+            //variables declaration
+            IWebDriver driver; //generic browser
+            string _browserType = args[0]; //get the command line argument for browser type
+            _browserType = _browserType.ToLower(); //convert to all lower case
 
+            Console.WriteLine(_browserType );
+
+            ////##Debug: iterate through CL arguments
+            //foreach (string arg in args)
+            //{ Console.WriteLine(arg);
+            //}//end foreach
+            
+           
             //find out which driver is chosen
-            if (driver.GetType().ToString().Contains("Chrome"))
+            if (_browserType == "chrome")
             {
+                driver = new ChromeDriver();
                 driver.Navigate().GoToUrl("http://www.gmail.com");
-                
+                driver.Close();
+
             }
 
-            else if (driver.GetType().ToString().Contains("InternetExplorer"))
+            else if (_browserType =="internetexplorer")
             {
+                driver = new InternetExplorerDriver();
                 driver.Navigate().GoToUrl("http://www.hotmail.com");
+                driver.Close();
             }
 
-            else if (driver.GetType().ToString().Contains("Firefox"))
+            else if (_browserType=="firefox")
             {
+                driver = new FirefoxDriver();
                 driver.Navigate().GoToUrl("http://www.yahoo.com");
+                driver.Close();
             }
 
-            driver.Close();
+            
 
         }
     }
