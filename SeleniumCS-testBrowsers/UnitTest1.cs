22b6f611b169553272a0313b3a0571fb8dba0f81
kraftwerk1611 <kraftwerk1611@gmail.com>
2017-12-02 12:10:46 -0500

    added testGoogle() method

--- SeleniumCS-testBrowsers/UnitTest1.cs
+++ SeleniumCS-testBrowsers/UnitTest1.cs
@@ -29,6 +29,24 @@ namespace SeleniumCS_testBrowsers
         }
 
     }//end  class
+
+    [Parallelizable]
+    [TestFixture]
+    public class GoogleTesting : Hooks
+    {
+        [Test]
+        public void testGoogle()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("Selenium");
+            driver.FindElement(By.Name("btnK")).Submit();
+            System.Threading.Thread.Sleep(5000);
+            Assert.That(driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                                            "The text selenium doest not exist");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(7);
+        }
+
+    }//end  class
 }
 
   
